de003dfaab8029a22ba8b6e5c30425f91544e152
Victor Duran <victor.duran.em@gmail.com>
2023-04-11 17:35:57 -0500

    Add basic chat code

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+
+  function onSend(newMessages = []) {
+    // TODO: Implement the logic to send the user's message to the GPT Assistant
+    // and receive the Assistant's response.
+
+    // For now, we will just echo the user's message back.
+    const assistantResponse = {
+      ...newMessages[0],
+      _id: Math.round(Math.random() * 1000000),
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: 'GPT Assistant',
+        avatar: 'https://placeimg.com/140/140/any',
+      },
+      text: `You said: ${newMessages[0].text}`,
+    };
+
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, [assistantResponse, newMessages[0]]),
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={(newMessages) => onSend(newMessages)}
+      user={{
+        _id: 1,
+      }}
+      inverted={true}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
